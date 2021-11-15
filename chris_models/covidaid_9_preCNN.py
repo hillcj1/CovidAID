c13da0cdfed946fdbc9cb2984019af69e2bc55d2
hillcj1 <91169636+hillcj1@users.noreply.github.com>
2021-11-14 19:37:28 -0500

    Add files via upload

---
+++ chris_models/covidaid_9_preCNN.py
@@ -0,0 +1,69 @@
+"""
+The main CovidAID and CheXNet implementation
+"""
+
+import torch
+import torch.nn as nn
+import torchvision
+import numpy as np
+
+class DenseNet121(nn.Module):
+    """Model modified.
+
+    The architecture of our model is the same as standard DenseNet121
+    except the classifier layer which has an additional sigmoid function.
+
+    """
+    def __init__(self, out_size):
+        super(DenseNet121, self).__init__()
+        self.densenet121 = torchvision.models.densenet121(pretrained=True)
+        num_ftrs = self.densenet121.classifier.in_features
+
+        self.conv1 = torch.nn.Conv2d(1000, 128, 2, 2)
+        self.dropout = torch.nn.Dropout(0.5)
+        self.relu = torch.nn.ReLU()
+        self.batch1 = torch.nn.BatchNorm2d(128)
+
+        self.conv2 = torch.nn.Conv2d(128, 64, 2, 2)
+        self.batch2 = torch.nn.BatchNorm2d(128)
+
+        self.fc1 = torch.nn.Linear(64, 128)
+        self.fc2 = torch.nn.Linear(128, out_size)
+        self.sig = torch.nn.Sigmoid()
+
+        self.densenet121.classifier = nn.Sequential(
+            nn.Linear(num_ftrs, out_size),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.dropout(x)
+        x = self.relu(x)
+        x = self.batch1(x)
+
+        x = self.conv2(x)
+        x = self.dropout(x)
+        x = self.relu(x)
+        x = self.batch2(x)
+
+        x = self.densenet121(x)
+        return x
+    
+
+class CovidAID(DenseNet121):
+    """
+    Modified DenseNet network with 4 classes
+    """
+    def __init__(self, combine_pneumonia=False):
+        NUM_CLASSES = 3 if combine_pneumonia else 4
+        super(CovidAID, self).__init__(NUM_CLASSES)
+
+
+class CheXNet(DenseNet121):
+    """
+    Modified DenseNet network with 14 classes
+    """
+    def __init__(self):
+        super(CheXNet, self).__init__(14)
